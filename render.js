6100bb764417df66af8c1e0bd06aa8423ababf0d
Aaron Kotz <156543340+Akotz89@users.noreply.github.com>
2026-05-30 11:12:45 -0500

    Interpolate organism rendering between simulation ticks

--- render.js
+++ render.js
@@ -10,6 +10,11 @@ function drawPixel(x, y, color) {
   );
 }
 
+function drawPixelAtCanvasPosition(canvasX, canvasY, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(canvasX, canvasY, CONFIG.TILE_SIZE, CONFIG.TILE_SIZE);
+}
+
 function buildTerrainCache() {
   terrainCache = document.createElement("canvas");
   terrainCache.width = canvas.width;
@@ -63,9 +68,28 @@ function getOrganismColor(organism) {
 }
 
 function drawOrganisms() {
+  var interpolation = world.interpolation;
+
+  if (interpolation < 0) {
+    interpolation = 0;
+  }
+
+  if (interpolation > 1) {
+    interpolation = 1;
+  }
+
   for (var i = 0; i < world.organisms.length; i++) {
     var organism = world.organisms[i];
-    drawPixel(organism.x, organism.y, getOrganismColor(organism));
+    var previousX = typeof organism.prevX === "number" ? organism.prevX : organism.x;
+    var previousY = typeof organism.prevY === "number" ? organism.prevY : organism.y;
+    var renderX = previousX + (organism.x - previousX) * interpolation;
+    var renderY = previousY + (organism.y - previousY) * interpolation;
+
+    drawPixelAtCanvasPosition(
+      renderX * CONFIG.TILE_SIZE,
+      renderY * CONFIG.TILE_SIZE,
+      getOrganismColor(organism)
+    );
   }
 }
 
